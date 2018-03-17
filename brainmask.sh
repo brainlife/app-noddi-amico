384b690c3cd893f1822545dc628005e6dcc4fe74
Brad Caron <bacaron@imail.iu.edu>
2018-03-16 20:09:16 -0400

    changes for registration

--- brainmask.sh
+++ brainmask.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-noddiImages="FIT_ICVF_NEW.nii.gz FIT_ISOVF_NEW.nii.gz FIT_OD_NEW.nii.gz FIT_dir.nii.gz config.pickle";
-noddiFile="NODDI";
-num="1 2 3";
-mkdir $noddiFile;
-
-
 echo "Setting file paths"
 # Grab the config.json inputs
 dwi=`jq -r '.dwi' config.json`;
 bvals=`jq -r '.bvals' config.json`;
 bvecs=`jq -r '.bvecs' config.json`;
+dtiinitDwi=`jq -r '.dtiinitDwi' config.json`;
+dtiinitBvals=`jq -r '.dtiinitBvals' config.json`;
 echo "Files loaded"
 
 # Create b0
 select_dwi_vols \
-	${dwi} \
+	dwi.nii.gz \
 	${bvals} \
 	nodif.nii.gz \
 	0;
@@ -27,17 +23,5 @@ bet nodif.nii.gz \
 	-g 0 \
 	-m;
 
-for NUM in $num
-	do
-		sed -n "${NUM}p" ${bvecs} > row_${NUM}.txt
-		fmt -1 row_${NUM}.txt > col_${NUM}.txt
-		fmt -1 ${bvals} > col_bvals.txt
-	done
-pr -m -t col_1.txt col_2.txt col_3.txt col_bvals.txt > bvals.scheme
-rm -rf *col* *row*
-cp -v nodif_brain_mask.nii.gz ./NODDI/
-cp -v bvals.scheme ./NODDI/
-cp -v ${dwi} ./NODDI/dwi.nii.gz
+echo "brainmask creation complete"
 
-
-echo "b0 brainmask creation complete"
