43d550768d6915a90b8706c8a1d89803592020ef
bacaron <bacaron@imail.iu.edu>
2018-05-26 06:10:16 -0400

    Update NODDI.py

--- NODDI.py
+++ NODDI.py
@@ -9,18 +9,18 @@ with open('config.json') as config_json:
 	config = json.load(config_json)
 	dPar = float(config['dPar'])
 	
-def NODDI():
-	import amico
-	cwd = os.getcwd()
-	amico.core.setup()
-	ae = amico.Evaluation(cwd,"NODDI")
-	ae.load_data(dwi_filename = "dwi.nii.gz", scheme_filename = "bvals.scheme", mask_filename = "nodif_brain_mask.nii.gz", b0_thr = 1)
-	ae.set_model("NODDI")
-	ae.model.dPar = dPar
-	ae.generate_kernels( regenerate = True )
-	ae.load_kernels()
-	ae.fit()
-	ae.save_results()
-	print("NODDI model generation complete")
+	def NODDI():
+		import amico
+		cwd = os.getcwd()
+		amico.core.setup()
+		ae = amico.Evaluation(cwd,"NODDI")
+		ae.load_data(dwi_filename = "dwi.nii.gz", scheme_filename = "bvals.scheme", mask_filename = "nodif_brain_mask.nii.gz", b0_thr = 1)
+		ae.set_model("NODDI")
+		ae.model.dPar = dPar
+		ae.generate_kernels( regenerate = True )
+		ae.load_kernels()
+		ae.fit()
+		ae.save_results()
+		print("NODDI model generation complete")
 
-NODDI()
+	NODDI()
