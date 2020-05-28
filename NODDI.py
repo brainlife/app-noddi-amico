0a585c89f7098869145943a5381f1ccb27ad74ed
Brad Caron <bacaron@iu.edu>
2020-05-28 16:43:31 -0400

    Update NODDI.py

--- NODDI.py
+++ NODDI.py
@@ -41,7 +41,7 @@ ae.set_model("NODDI") #this creates solver_params
 
 ae.model.dPar = dPar
 ae.model.IC_VFs = np.linspace(0.0,1,12)
-ae.generate_kernels()
+ae.generate_kernels( regenerate = True )
 
 #https://github.com/daducci/AMICO/issues/67
 ae.CONFIG['solver_params']['numThreads'] = nb_threads
